6e94a43c99f5477ee8c3446f3827bd4e060d738f
Phil Lennon <me@phil.dev>
2020-09-17 12:27:01 +0100

    change availability to now if date is in the past

--- src/_utils/serializers.js
+++ src/_utils/serializers.js
@@ -1,12 +1,21 @@
+const isPast = require('date-fns/isPast')
+const parseISO = require('date-fns/parseISO')
 const blocksToHtml = require('@sanity/block-content-to-html')
 const h = blocksToHtml.h
 
 const serializers = {
   types: {
     dateAvailableReference: (props) => {
-      return h('span', {className: 'js-available-date font-bold'}, props.node.date)
+      let theText
+      if (isPast(parseISO(props.node.date))) {
+        theText = 'Now'
+      } else {
+        theText = props.node.date
+      }
+
+      return h('span', {className: 'js-available-date font-bold'}, theText)
     },
-    dateAvailableStarted: (props) => {
+    dateStartedReference: (props) => {
       return h('span', {className: 'js-started-date font-bold'}, props.node.date)
     },
   }
