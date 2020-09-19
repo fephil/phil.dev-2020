1a607cef697bdc39493961c3d3567e321a835b3d
Phil Lennon <me@phil.dev>
2020-09-19 14:34:37 +0100

    add sentry

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
 import startDayCalculator from './modules/startDayCalculator.js';
+
+Sentry.init({
+  dsn: 'https://26409a2261d447b6b5827e9b6469c1fc@o77665.ingest.sentry.io/5434439',
+  release: 'phil.dev-2020@' + projectVersion,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 0.5,
+});
 
 startDayCalculator();
