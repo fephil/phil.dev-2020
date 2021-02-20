12a004db4b010b2f941fa922cc85a3f0adc271e3
Phil Lennon <me@phil.dev>
2021-02-20 16:49:46 +0000

    redesigned footer

--- src/_data/footer.js
+++ src/_data/footer.js
@@ -4,7 +4,6 @@ const client = require('../_utils/sanityClient.js');
 async function getFooter() {
   const filter = groq`*[_type == "footer"]`;
   const projection = groq`{
-    about,
     business
   }[0]`;
   const query = [filter, projection].join(' ');
